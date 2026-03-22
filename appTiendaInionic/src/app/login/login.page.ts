5a1f97bcf97ffc2b492b038aa639d395a780edde
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2026-03-22 18:16:22 -0500

    hola

--- appTiendaInionic/src/app/login/login.page.ts
+++ appTiendaInionic/src/app/login/login.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonCard, IonCardHeader, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonCard, IonCardHeader, IonButton, IonFooter, IonButtons } from '@ionic/angular/standalone';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonLabel, IonItem, IonCard, IonCardHeader, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonLabel, IonItem, IonCard, IonCardHeader, IonButton, IonFooter, IonButtons, RouterModule]
 })
 export class LoginPage implements OnInit {
   formularioLogin: FormGroup;
--- appTiendaInionic/src/app/registro/registro.page.ts
+++ appTiendaInionic/src/app/registro/registro.page.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent,
+    IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton,
+    IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonLabel, IonButton]
 })
 export class RegistroPage implements OnInit {
+  formularioRegistro: FormGroup;
+  constructor(public fb: FormBuilder) {
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", [Validators.required]),
+      'confirmacionPassword': new FormControl("", [Validators.required]),
+    })
 
-  constructor() { }
+
+
+
+  }
 
   ngOnInit() {
   }
