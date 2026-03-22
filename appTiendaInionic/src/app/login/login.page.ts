6cbba6c5e4ae721cfaf6cbaf1185303c07e2d261
AggiePaola <104395719+AggiePaola@users.noreply.github.com>
2026-03-22 16:39:35 -0500

    boton ingresar

--- appTiendaInionic/src/app/login/login.page.ts
+++ appTiendaInionic/src/app/login/login.page.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonCard, IonCardHeader, IonButton } from '@ionic/angular/standalone';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule, IonLabel, IonItem, IonCard, IonCardHeader, IonButton]
 })
 export class LoginPage implements OnInit {
+  formularioLogin: FormGroup;
 
-  constructor() { }
+
+  constructor(public fb: FormBuilder) {
+    this.formularioLogin = this.fb.group(
+      {
+        'nombre': new FormControl("", Validators.required),
+        'password': new FormControl("", Validators.required),
+      }
+    )
+  }
 
   ngOnInit() {
   }
